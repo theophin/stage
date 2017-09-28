4174e0de19a0441e3fa5d88c2fef937f8eeb3182
theophin <31925254+theophin@users.noreply.github.com>
2017-09-28 09:32:38 +0200

    fichiers effacÃs

--- fonctions.py
+++ fonctions.py
@@ -1,9 +1,13 @@
 import csv ,os
 
-def Lisible(param1):   
+def Lisible(param1,type):   
+  if(type==1) :
     fichier = param1+".csv"
+  else : 
+    fichier=param1
+
     with open (fichier, encoding='utf8',errors= 'replace') as csv_file :
-      with open(param1+"1.csv",'w') as file:
+      with open("out",'w') as file:
         for line in csv_file :
               fields= line.split(',') 
               fields='.'.join(fields)
@@ -11,7 +15,13 @@ def Lisible(param1):
               file.write(','.join(ret))
               
     os.remove(fichier)
-    os.rename(param1+"1.csv",fichier)
+    os.rename("out",fichier)
 
 def CalculeFreq(param1,param2) :
     return (1/(param2-param1))
+
+def  essai(nom):
+	
+	if ("toto"in nom):
+		print("nice")
+	
